488069fb6ef792b47f3497dd52e97e340aca9b15
Cleclemi28 <c.trofleau@gmail.com>
2017-06-08 12:51:23 +0200

    mise en place de la page home

--- app/js/controllers/registerController.js
+++ app/js/controllers/registerController.js
@@ -1,8 +1,10 @@
 angular.module('app')
     .controller('RegisterController', function($scope, $state, Auth) {
+      $scope.user = {};
         $scope.register = function() {
             Auth.register($scope.user).then(function() {
                 $state.go('anon.home');
             });
         };
+
     });
